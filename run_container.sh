f90907754f0c05b74dc7c116c3449e641d915fca
root <root@vmi1151459.contaboserver.net>
2023-09-24 21:05:40 -0400

    add: run_contianer.sh

---
+++ run_container.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/sh
+
+echo -n "Enter the name(also username) for the container: "
+read username
+echo -n "Enter the start port for the range of ports(first port is also used for ssh): "
+read start_port
+echo -n "Enter the last port for the range of ports: "
+read end_port
+echo -n "Enter container image name: "
+read img_name
+echo -n "Enter container run command (i.e bash, custom script, etc.): "
+read cmd_name
+password=$(head -c 30 /dev/urandom | sha256sum | cut -f 1 -d " ")
+docker volume create -d local -o size=1g $username
+docker run -d -it \
+	--name=$username \
+	--hostname=$username `#optional` \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=America/NewYork \
+  -e SUDO_ACCESS=true `#optional` \
+  -e PASSWORD_ACCESS=true `#optional` \
+  -e USER_PASSWORD=$password \
+  -e USER_NAME=$username `#optional` \
+  --cpuset-cpus="0" \
+  --memory=512M \
+  --pids-limit=20000 \
+  --storage-opt size=5g \
+  --domainname=$username \
+  -p $start_port:22 \
+  -p $((start_port+1))-$end_port:$((start_port+1))-$end_port \
+  --restart unless-stopped \
+	go_debian-ssh bash
+
+echo "Docker SSH created successfully copy password below to login into your ssh session"
+echo $password
